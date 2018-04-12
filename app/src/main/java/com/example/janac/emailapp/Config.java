c93da1037caff8b9f32ce959b424bf734f145a71
ChandanJana <chandanjana@gmail.com>
2018-04-12 21:55:07 +0530

    Initial commit

---
+++ app/src/main/java/com/example/janac/emailapp/Config.java
@@ -0,0 +1,11 @@
+package com.example.janac.emailapp;
+
+
+/**
+ * Created by janac on 28-03-2018.
+ */
+
+public class Config {
+    public final static String EMAIL ="MCA.4.BIT@gmail.com";
+    public final static String PASSWORD ="mca4500115";
+}
